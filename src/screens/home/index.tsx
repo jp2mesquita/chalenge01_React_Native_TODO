be361c44655d2d0ee7349725da29f85840cce121
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-03 21:05:04 -0300

    feat: logic to control the tasks

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,10 +1,135 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { Image, SafeAreaView, Text, TextInput, TouchableOpacity, View} from "react-native";
+import { Empty } from "../../components/Empty";
+import { Header } from "../../components/Header";
+import { ListHeader } from "../../components/ListHeader";
+import { Task } from "../../components/Task";
 import { styles } from "./styles";
 
+export interface TaskListProps{
+  description: string
+  isDone: boolean,
+}
+
 export function Home(){
+  const [ taskList, setTaskList ] = useState<TaskListProps[]>([])
+  const [newTask, setNewTask] = useState<string>('')
+  // const [ doneTasksAmount, setDoneTasksAmount ] = useState(0)
+  const [ createdTaksAmount, setCreatedTasksAmount ] = useState(0)
+
+  let count = 0
+  taskList.forEach(task => {
+    if(task.isDone){
+      count++
+    }
+  });
+  const doneTasksAmount = count
+
+  function handleNewTaskInput(text: string){
+    setNewTask(text)
+  }
+
+  function handleAddNewTask(){
+    const taskToAdd = {
+      description: newTask,
+      isDone: false
+    }
+    setTaskList(prevState => [...prevState, taskToAdd])
+    setNewTask('')
+    const prevListLength = taskList.length
+    setCreatedTasksAmount(prevListLength + 1)
+  }
+
+  function handleDeleteTask(taskToDelete: string){
+    const taskListWithouDeleteOne = taskList.filter(task => task.description!=taskToDelete)
+    setTaskList(taskListWithouDeleteOne)
+    const prevListLength = taskList.length
+    setCreatedTasksAmount(prevListLength - 1)
+  }
+
+  function handleToggleIsDone(taskToToggle: string){
+    setTaskList(prevState => prevState.map(task => {
+      if(task.description != taskToToggle){
+        return( task )
+      }else{
+        const taskWithCheckToggle = {
+          description: taskToToggle,
+          isDone: !task.isDone
+        }
+        return(taskWithCheckToggle)
+      }
+    }))
+  }
+
   return(
-    <View style={styles.constainer}>
-      <Text style={styles.title}>Hello World</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Header />
+      
+      <View style={styles.main}>
+        <View style={styles.inputArea}>
+          <TextInput 
+            style={styles.textInput}
+            placeholder="Adicione  uma nova tarefa"  
+            placeholderTextColor={'#808080'}
+            onChangeText={(text) => handleNewTaskInput(text)}
+            value={newTask}
+          />
+          
+          <TouchableOpacity 
+            style={styles.button}
+            onPress={handleAddNewTask}
+          >
+            <Image 
+              source={require('../../assets/plus.png')}
+            />
+          </TouchableOpacity>
+        </View>
+
+        <ListHeader  
+          createdTaksAmount={createdTaksAmount}
+          doneTasksAmount={doneTasksAmount}
+
+        />
+
+        <View style={styles.listContainer}>
+      
+          {taskList.length > 0 
+            ? 
+              <>
+                {
+                  taskList.map(task => {
+                    if(!task.isDone){
+                      return (
+                        <Task
+                          key={task.description}
+                          task={task}
+                          onDelete={handleDeleteTask}
+                          onToggle={handleToggleIsDone}
+                        />
+                      )
+                    }
+                  })
+                }
+                {
+                  taskList.map(task => {
+                    if(task.isDone){
+                      return (
+                        <Task
+                          key={task.description}
+                          task={task}
+                          onDelete={handleDeleteTask}
+                          onToggle={handleToggleIsDone}
+                        />
+                      )
+                    }
+                  })
+                }
+              </>
+            : <Empty />
+          }
+
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
--- src/screens/home/styles.ts
+++ src/screens/home/styles.ts
@@ -1,12 +1,58 @@
 import { StyleSheet } from "react-native";
+import { Platform, StatusBar } from "react-native";
+
+const isAndroid = Platform.OS === 'android'
+const height = isAndroid ? StatusBar.currentHeight : 0
 
 export const styles = StyleSheet.create({
-  constainer:{
+  container:{
+    paddingTop: height,
     flex: 1,
-    backgroundColor: 'gray',
-    padding: 24
+    backgroundColor: '#0d0d0d',
+    
   },
-  title:{
-    color: '#FFF',
+
+  main:{
+    flex: 1,
+    backgroundColor: '#1A1A1A',
+    paddingHorizontal: 24
+  },
+
+  inputArea:{
+    height: 54,
+    marginTop: -27,
+    flexDirection: 'row',
+  },
+
+  textInput:{
+    flex: 1,
+    backgroundColor: "#262626",
+    borderColor: '#0d0d0d',
+    borderWidth: 1,
+    borderRadius: 8,
+    padding: 16,
+    color: '#f2f2f2',
+  },
+
+  button:{
+    height: 54,
+    width: 52,
+    backgroundColor: '#1e6f9f',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 6,
+    padding: 18,
+    marginLeft: 8,
+  },
+
+  listContainer:{
+    marginTop: 20,
+    paddingVertical: 48,
+    
+    borderTopWidth: 2,
+    borderRadius: 12,
+    borderColor: '#333333',
+    alignItems: 'center',
+    justifyContent: 'center'
   }
 })
