0b1148e3c2931314b19beabea531a7065110a3cd
João Paulo Mesquita <jp-mesquita@live.com>
2022-12-04 17:22:07 -0300

    fix: change the margin top  to empty component

--- src/components/Empty/styles.ts
+++ src/components/Empty/styles.ts
@@ -3,7 +3,11 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container:{
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    paddingVertical: 48,
+    borderTopWidth: 2,
+    borderRadius: 6,
+    borderColor: '#333333',
   }, 
   
 })
